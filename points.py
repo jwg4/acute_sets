c881707373cf66691353ab7edc76b7a6f2e787bd
Jack Grahl <jack@prismfp.com>
2018-07-13 12:27:43 +0100

    Move all this code into its own module.

--- points.py
+++ points.py
@@ -1,37 +1,6 @@
 from random import randint
 
-
-def check_angle(a, b, c):
-    d = len(a)
-    s = 0
-    for i in range(0, d):
-        print (a[i] - b[i], c[i] - b[i])
-        s = s + (a[i] - b[i]) * (c[i] - b[i])
-    return s > 0
-
-
-def check_triplet(a, b, c):
-    return check_angle(a, b, c) and check_angle(b, c, a) and check_angle(c, a, b)
-
-
-def check_new_point(point, points):
-    n = len(points)
-    for i in range(0, n):
-        for j in range(i + 1, n):
-            if not check_triplet(point, points[i], points[j]):
-                return False
-    return True
-
-
-def check_whole_set(points):
-    n = len(points)
-    for i in range(0, n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
-                if not check_triplet(points[i], points[j], points[k]):
-                    print(points[i], points[j], points[k])
-                    return False
-    return True
+from checking import check_new_point
 
 
 def generate_random_point(dimension, limit=65535):
