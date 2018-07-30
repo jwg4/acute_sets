d1c7adad30bab37da4a936feb14d89167d3f4a90
Jack Grahl <jack@prismfp.com>
2018-07-30 18:04:55 +0100

    Use tuples and booleans.

--- greedy_cubc.py
+++ greedy_cubc.py
@@ -1,3 +1,4 @@
+import logging
 import random
 
 
@@ -16,7 +17,7 @@ def forbidden_third(a, b):
         if a[i] == True and b[i] == False:
             x[i] = False
             y[i] = True
-    return [x, y]
+    return [tuple(x), tuple(y)]
 
 
 def generate_matching(l):
@@ -28,10 +29,10 @@ def generate_matching(l):
         yield l
     else:
         k = l.index(None)
-        x0 = l[:k] + [False] + l[k+1:]
+        x0 = tuple(l[:k] + (False,) + l[k+1:])
         for m in generate_matching(x0):
             yield m
-        x1 = l[:k] + [True] + l[k+1:]
+        x1 = tuple(l[:k] + (True,) + l[k+1:])
         for m in generate_matching(x1):
             yield m
 
@@ -46,33 +47,38 @@ def gen_forbidden_set(vs):
 
 
 def forbidden_set(vs):
-    return list(gen_forbidden_set(vs))
+    return set(gen_forbidden_set(vs))
 
 
 def find_small_overlap(current, target):
     n = len(target[0])
     forbidden = forbidden_set(current)
-    expanded = [v + [1] for v in target]
+    logging.info("Dimension is %d." % (n, ))
+    logging.info("Size of current set is %d." % (len(current), ))
+    logging.info("Size of forbidden set is %d." % (len(forbidden), ))
     mx = 0
-    candidate = None
+    candidate = []
     for i in range(0, 10000):
         flips = [random.choice([True, False]) for t in range(0, n)]
-        flipped_target = [ [(a != b) for (a, b) in zip(flips, t)] for t in target ] 
+        flipped_target = [ tuple((a != b) for (a, b) in zip(flips, t)) for t in target ] 
         non_overlap = [f for f in flipped_target if f not in forbidden]
         c = len(non_overlap)
         if c > mx:
             mx = c
             candidate = non_overlap
-    if candidate:
-        return [[0] + g for g in current] + [[1] + g for f in candidate]
+    return [tuple((False,) + g) for g in current] + [tuple((True,) + g) for f in candidate]
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
     initial = [
-        [False, False, False],
-        [False, True, True],
-        [True, False, True],
-        [True, True, False],
+        (False, True, True),
+        (True, False, True),
+        (True, True, False),
     ]
 
-    print(find_small_overlap(initial, initial))
+    s = initial
+    for i in range(0, 5):
+        s = find_small_overlap(s, s)
+        print(s)
